89cff59d33378ead4d1aab69fb324e02ba230485
Humberto Alcocer <humbertowoody@gmail.com>
2018-10-14 13:38:12 -0500

    Update dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,9 +1,17 @@
+const Player = require('../models/Player');
+
 /**
  * GET /dashboard
  * Get the dashboard page.
  */
 exports.getDashboard = (req, res, next) => {
-  res.render('admin/dashboard', {
-    title: 'Dashboard'
-  });
+  Player.countDocuments()
+    .exec()
+    .then((totalPlayers) => {
+      res.render('admin/dashboard', {
+        title: 'Dashboard',
+        totalPlayers
+      });
+    })
+    .catch(err => next(err));
 };
